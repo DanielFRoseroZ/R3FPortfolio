53f8754d75b6d9132276ff141f4139c4eca2b491
Daniel Felipe Rosero Zemanate <dandan32099@gmail.com>
2023-10-08 11:05:38 -0500

    Some fixes

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -143,16 +143,43 @@ const lenguages = [
 const SkillsSection = () => {
     return (
         <Section>
-            <div>
+            <motion.div whileInView={"visible"}>
                 <h2 className="text-5xl font-bold">Skills</h2>
                 <div className=" mt-8 space-y-4">
                     {skills.map((skill, index) => (
                         <div className="w-64" key={index}>
-                            <h3 className="text-xl font-bold" text-gray-800>{skill.title}</h3>
+                            <motion.h3 className="text-xl font-bold" text-gray-800
+                                initial={{
+                                    opacity: 0,
+                                }}
+                                variants={{
+                                    visible: {
+                                        opacity: 1,
+                                        transition:{
+                                            duration: 1,
+                                            delay: 1 + index * 0.2,
+                                        }
+                                    }
+                                }}
+                            >{skill.title}</motion.h3>
                             <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                                <div
+                                <motion.div
                                     className="h-full bg-green-500 rounded-full"
                                     style={{ width: `${skill.level}%` }}
+                                    initial={{
+                                        scaleX: 0,
+                                        originX: 0,
+                                    }}
+                                    variants={{
+                                        visible: {
+                                            scaleX: 1,
+                                            transition:{
+                                                duration: 1,
+                                                delay: 1 + index * 0.2,
+                                            }
+                                        }
+                                    }}
+
                                 />
                             </div>
                         </div>
@@ -163,18 +190,44 @@ const SkillsSection = () => {
                     <div className=" mt-8 space-y-4">
                         {lenguages.map((lenguage, index) => (
                             <div className="w-64" key={index}>
-                                <h3 className="text-xl font-bold" text-gray-800>{lenguage.title}</h3>
+                                <motion.h3 className="text-xl font-bold" text-gray-800
+                                    initial={{
+                                        opacity: 0,
+                                    }}
+                                    variants={{
+                                        visible: {
+                                            opacity: 1,
+                                            transition:{
+                                                duration: 1,
+                                                delay: 2 + index * 0.2,
+                                            }
+                                        }
+                                    }}
+                                >{lenguage.title}</motion.h3>
                                 <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                                    <div
+                                    <motion.div
                                         className="h-full bg-green-500 rounded-full"
                                         style={{ width: `${lenguage.level}%` }}
+                                        initial={{
+                                            scaleX: 0,
+                                            originX: 0,
+                                        }}
+                                        variants={{
+                                            visible: {
+                                                scaleX: 1,
+                                                transition:{
+                                                    duration: 1,
+                                                    delay: 2 + index * 0.2,
+                                                }
+                                            }
+                                        }}
                                     />
                                 </div>
                             </div>
                         ))}
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </Section>
     )
 }
